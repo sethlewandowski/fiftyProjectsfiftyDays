e15e17391d8f2c48cc0dfb8ed18dcb6de4e19508
Seth Lewandowski <sethlew@protonmail.com>
2021-03-01 15:44:27 -0700

    finished faq project

--- faq/script.js
+++ faq/script.js
@@ -1,8 +1,10 @@
 // first bring in all buttons
 const faqs = document.querySelectorAll('.faq-toggle')
 
+// now loop through entire list of buttons and add a click listener
 faqs.forEach((faq) => {
   faq.addEventListener('click', () => {
+    // when button gets clicked, toggle the active class
     faq.parentNode.classList.toggle('active')
   })
 })
